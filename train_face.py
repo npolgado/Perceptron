04fd8aa325a938edcea1d9a4f71a4d1377123802
Nicholas Olgado <nicholas831@gmail.com>
2022-12-28 02:39:03 -0800

    moving the network to a python object file

--- train_face.py
+++ train_face.py
@@ -1,199 +1,85 @@
-#!usr/env/python3
-import cv2
-import time
+#!usr/bin/env python3
 import os
-import datetime
+import time
+
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from tkinter import filedialog
+
+from perceptron import *
 
 # Load the Haar cascade for face detection
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 video_capture = cv2.VideoCapture(0)
 
 RESIZE_FACTOR = 512
-ROUNDING_AMOUNT = 5
 TRAIN = True
 
-class Perceptron:
-    def __init__(self, learning_rate=0.1, weights=None, bias=20):
-        """
-        Initialize the perceptron with the given learning rate and weights
-        """
-        self.learning_rate = learning_rate
-
-        self.save_path = os.path.dirname(__file__)
-        self.load_weights(weights)
-
-        self.bias = bias
-        self.adjusted = 0
-        self.accuracy = 0
-        self.loss = 0
-        self.correct_guesses = 0
-        self.wrong_guesses = 0
-
-        self.max_weight = 0
-        self.min_weight = 10000
-        print(f"weights: {self.weights.shape}")
-
-    def load_weights(self, weights):
-        path = filedialog.askopenfilename()
-        if os.path.exists(path):
-            self.weights = np.loadtxt(path)
-            print(self.weights)
-            self.save_path = path
-        else:
-            self.weights = weights
-            self.save_path = None
-
-    def add_inputs(self, input):
-        for i in range(len(self.weights)):
-            for j in range(len(self.weights[i])):
-                self.weights[i][j] += input[i][j]
-
-    def subtract_inputs(self, input):
-        for i in range(len(self.weights)):
-            for j in range(len(self.weights[i])):
-                self.weights[i][j] -= input[i][j]
-
-    def train(self, inputs, label):
-        """
-        Train the perceptron weights based on the given inputs and labels
-        """
-        prediction = self.predict(inputs)
-        print_pred = round(prediction, ROUNDING_AMOUNT)
-
-        bool_prediction = bool(0)
-        
-        if prediction > self.bias:
-            bool_prediction = True
-
-            if bool_prediction == bool(label):
-                self.correct_guesses += 1
-            else:
-                self.wrong_guesses += 1
-                self.subtract_inputs(inputs)
-                self.adjusted += 1
-                
-        else:
-            bool_prediction = False
-
-            if bool_prediction == bool(label):
-                self.correct_guesses += 1
-            else:
-                self.wrong_guesses += 1
-                self.add_inputs(inputs)
-                self.adjusted += 1
-
-        self.accuracy = round(float((self.correct_guesses / (self.wrong_guesses + self.correct_guesses))*100), ROUNDING_AMOUNT)
-        self.loss = round(float(100 - self.accuracy), ROUNDING_AMOUNT)
-            
-    def predict(self, inputs):
-        """
-        Make a prediction based on the given inputs
-        """
-        output = float(0)
-        for i in range(len(inputs)):
-            for j in range(len(inputs[i])):
-                output += float(inputs[i][j] * self.weights[i][j])
-        
-        print(f"{self.adjusted}\t| +: {self.accuracy} %\t| -: {self.loss} %\t| {round(output, ROUNDING_AMOUNT)}\t| {bool(output > self.bias)}")
-        return output
-
-    def save_weights(self):
-        date = str(datetime.datetime.now().date())
-
-        max = 0
-        min = 100000
-        for i in range(len(self.weights)):
-            for j in range(len(self.weights[i])):
-                val = self.weights[i][j]
-                if val > max: max = val
-                elif val < min: min = val
-        
-        root = os.path.dirname(__file__)
-        weights_folder = os.path.join(root, "weights")
-        visuals_folder = os.path.join(weights_folder, "images")
-        if not os.path.exists(visuals_folder):
-            os.mkdir(visuals_folder)
-        fig_path = os.path.join(visuals_folder, f"{self.adjusted}_{date}.png")
-        raw_path = os.path.join(weights_folder, f"{date}.txt")
-
-        if self.save_path:
-            if os.path.exists(self.save_path):
-                weights_file_name = os.path.split(self.save_path)[1].split(".")[0]
-                raw_path = self.save_path
-                fig_path = os.path.join(visuals_folder, f"{self.adjusted}_{weights_file_name}.png")
-
-        plt.imshow(self.weights, cmap='viridis', vmin=min, vmax=max)
-        plt.colorbar()
-
-        plt.savefig(fig_path)
-        np.savetxt(raw_path, self.weights, fmt='%.3f')
-
 try:
-    p = Perceptron(0.1, np.zeros((RESIZE_FACTOR, RESIZE_FACTOR)))
+    p = Perceptron(np.zeros((RESIZE_FACTOR, RESIZE_FACTOR)))
     st = time.monotonic()
     is_face = False
     root = os.path.dirname(__file__)
     training_folder = os.path.join(root, "faces")
     training_folder_truth = os.path.join(root, "my_face")
+    num_incorrect_passes = 1
+    num_truth_passes = 25
     while True:  
         if float(time.monotonic() - st) > 30:
             print("\t\tTRAINING ON _NOT_YOUR_FACE_")
-            for root, dirs, files in os.walk('./faces'):
-                for file in files:
+            for i in range(num_incorrect_passes):
+                for root, dirs, files in os.walk('./faces'):
+                    for file in files:
 
-                    im = cv2.imread(os.path.join(training_folder, file))
-                    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                        im = cv2.imread(os.path.join(training_folder, file))
+                        im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-                    faces = face_cascade.detectMultiScale(im, 1.3, 5)
-                    bool_faces = len(faces)
-                    for (x, y, w, h) in faces:
-                        face = im[y:y+h, x:x+w]
+                        faces = face_cascade.detectMultiScale(im, 1.3, 5)
+                        bool_faces = len(faces)
+                        for (x, y, w, h) in faces:
+                            face = im[y:y+h, x:x+w]
 
-                        resized_image = cv2.resize(face, (RESIZE_FACTOR, RESIZE_FACTOR))
-                        normalized_image = resized_image / 255.0
-                        
-                        if TRAIN:
-                            p.train(normalized_image, 0.0)
-                        else:
-                            if p.predict(normalized_image) > p.bias:
-                                print("False!")
+                            resized_image = cv2.resize(face, (RESIZE_FACTOR, RESIZE_FACTOR))
+                            normalized_image = resized_image / 255.0
+                            
+                            if TRAIN:
+                                p.train(normalized_image, 0.0)
                             else:
-                                print("True!")
-                    if not bool_faces:
-                        resized_image = cv2.resize(im, (RESIZE_FACTOR, RESIZE_FACTOR))
-                        normalized_image = resized_image / 255.0
-                        p.train(normalized_image, 0.0)
+                                if p.predict(normalized_image) > p.bias:
+                                    print("False!")
+                                else:
+                                    print("True!")
+                        if not bool_faces:
+                            resized_image = cv2.resize(im, (RESIZE_FACTOR, RESIZE_FACTOR))
+                            normalized_image = resized_image / 255.0
+                            p.train(normalized_image, 0.0)
 
             print("\t\tTRAINING ON _YOUR_FACE_")
-            for root, dirs, files in os.walk('./my_face'):
-                for file in files:
+            for i in range(num_truth_passes):
+                for root, dirs, files in os.walk('./my_face'):
+                    for file in files:
 
-                    im = cv2.imread(os.path.join(training_folder_truth, file))
-                    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                        im = cv2.imread(os.path.join(training_folder_truth, file))
+                        im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-                    faces = face_cascade.detectMultiScale(im, 1.3, 5)
-                    bool_faces = len(faces)
-                    for (x, y, w, h) in faces:
-                        face = im[y:y+h, x:x+w]
+                        faces = face_cascade.detectMultiScale(im, 1.3, 5)
+                        bool_faces = len(faces)
+                        for (x, y, w, h) in faces:
+                            face = im[y:y+h, x:x+w]
 
-                        resized_image = cv2.resize(face, (RESIZE_FACTOR, RESIZE_FACTOR))
-                        normalized_image = resized_image / 255.0
-                        
-                        if TRAIN:
-                            p.train(normalized_image, 1.0)
-                        else:
-                            if p.predict(normalized_image) > p.bias:
-                                print("False!")
+                            resized_image = cv2.resize(face, (RESIZE_FACTOR, RESIZE_FACTOR))
+                            normalized_image = resized_image / 255.0
+                            
+                            if TRAIN:
+                                p.train(normalized_image, 1.0)
                             else:
-                                print("True!")
-                    if not bool_faces:
-                        resized_image = cv2.resize(im, (RESIZE_FACTOR, RESIZE_FACTOR))
-                        normalized_image = resized_image / 255.0
-                        p.train(normalized_image, 0.0)
+                                if p.predict(normalized_image) > p.bias:
+                                    print("False!")
+                                else:
+                                    print("True!")
+                        if not bool_faces:
+                            resized_image = cv2.resize(im, (RESIZE_FACTOR, RESIZE_FACTOR))
+                            normalized_image = resized_image / 255.0
+                            p.train(normalized_image, 0.0)
 
             print("\t\tRESETING...\n")
             st = time.monotonic()
@@ -228,7 +114,8 @@ try:
             normalized_image = resized_image / 255.0
 
             if TRAIN:
-                p.train(normalized_image, 0.0)
+                pass
+                # print(f"guessing: {bool(p.predict(normalized_image) > p.bias)}")
             else:
                 if p.predict(normalized_image) > p.bias:
                     print("False!")
