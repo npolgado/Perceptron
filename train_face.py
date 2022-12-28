e48a606b2b191658dca1a1798715994d660dfb90
Nicholas Olgado <nicholas831@gmail.com>
2022-12-28 02:06:31 -0800

    added more photo training on dataset of your face (non webcam)

--- train_face.py
+++ train_face.py
@@ -12,13 +12,12 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 video_capture = cv2.VideoCapture(0)
 
-RESIZE_FACTOR = 256
+RESIZE_FACTOR = 512
 ROUNDING_AMOUNT = 5
 TRAIN = True
 
 class Perceptron:
-    
-    def __init__(self, learning_rate=0.1, weights=None, bias=0):
+    def __init__(self, learning_rate=0.1, weights=None, bias=20):
         """
         Initialize the perceptron with the given learning rate and weights
         """
@@ -68,7 +67,7 @@ class Perceptron:
         bool_prediction = bool(0)
         
         if prediction > self.bias:
-            bool_prediction = False
+            bool_prediction = True
 
             if bool_prediction == bool(label):
                 self.correct_guesses += 1
@@ -78,7 +77,7 @@ class Perceptron:
                 self.adjusted += 1
                 
         else:
-            bool_prediction = True
+            bool_prediction = False
 
             if bool_prediction == bool(label):
                 self.correct_guesses += 1
@@ -99,7 +98,7 @@ class Perceptron:
             for j in range(len(inputs[i])):
                 output += float(inputs[i][j] * self.weights[i][j])
         
-        print(f"{self.adjusted}\t| +: {self.accuracy} %\t| -: {self.loss} %\t| {round(output, ROUNDING_AMOUNT)}\t| {bool(output < self.bias)}")
+        print(f"{self.adjusted}\t| +: {self.accuracy} %\t| -: {self.loss} %\t| {round(output, ROUNDING_AMOUNT)}\t| {bool(output > self.bias)}")
         return output
 
     def save_weights(self):
@@ -115,13 +114,17 @@ class Perceptron:
         
         root = os.path.dirname(__file__)
         weights_folder = os.path.join(root, "weights")
-        fig_path = os.path.join(weights_folder, f"{date}.png")
+        visuals_folder = os.path.join(weights_folder, "images")
+        if not os.path.exists(visuals_folder):
+            os.mkdir(visuals_folder)
+        fig_path = os.path.join(visuals_folder, f"{self.adjusted}_{date}.png")
         raw_path = os.path.join(weights_folder, f"{date}.txt")
 
         if self.save_path:
             if os.path.exists(self.save_path):
+                weights_file_name = os.path.split(self.save_path)[1].split(".")[0]
                 raw_path = self.save_path
-                fig_path = os.path.join(os.path.split(raw_path)[0], f"{date}.png")
+                fig_path = os.path.join(visuals_folder, f"{self.adjusted}_{weights_file_name}.png")
 
         plt.imshow(self.weights, cmap='viridis', vmin=min, vmax=max)
         plt.colorbar()
@@ -130,13 +133,15 @@ class Perceptron:
         np.savetxt(raw_path, self.weights, fmt='%.3f')
 
 try:
-    p = Perceptron(0.1, np.random.rand(RESIZE_FACTOR, RESIZE_FACTOR))
+    p = Perceptron(0.1, np.zeros((RESIZE_FACTOR, RESIZE_FACTOR)))
     st = time.monotonic()
     is_face = False
     root = os.path.dirname(__file__)
     training_folder = os.path.join(root, "faces")
+    training_folder_truth = os.path.join(root, "my_face")
     while True:  
         if float(time.monotonic() - st) > 30:
+            print("\t\tTRAINING ON _NOT_YOUR_FACE_")
             for root, dirs, files in os.walk('./faces'):
                 for file in files:
 
@@ -144,7 +149,7 @@ try:
                     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
                     faces = face_cascade.detectMultiScale(im, 1.3, 5)
-
+                    bool_faces = len(faces)
                     for (x, y, w, h) in faces:
                         face = im[y:y+h, x:x+w]
 
@@ -158,11 +163,43 @@ try:
                                 print("False!")
                             else:
                                 print("True!")
+                    if not bool_faces:
+                        resized_image = cv2.resize(im, (RESIZE_FACTOR, RESIZE_FACTOR))
+                        normalized_image = resized_image / 255.0
+                        p.train(normalized_image, 0.0)
+
+            print("\t\tTRAINING ON _YOUR_FACE_")
+            for root, dirs, files in os.walk('./my_face'):
+                for file in files:
+
+                    im = cv2.imread(os.path.join(training_folder_truth, file))
+                    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+                    faces = face_cascade.detectMultiScale(im, 1.3, 5)
+                    bool_faces = len(faces)
+                    for (x, y, w, h) in faces:
+                        face = im[y:y+h, x:x+w]
+
+                        resized_image = cv2.resize(face, (RESIZE_FACTOR, RESIZE_FACTOR))
+                        normalized_image = resized_image / 255.0
+                        
+                        if TRAIN:
+                            p.train(normalized_image, 1.0)
+                        else:
+                            if p.predict(normalized_image) > p.bias:
+                                print("False!")
+                            else:
+                                print("True!")
+                    if not bool_faces:
+                        resized_image = cv2.resize(im, (RESIZE_FACTOR, RESIZE_FACTOR))
+                        normalized_image = resized_image / 255.0
+                        p.train(normalized_image, 0.0)
+
+            print("\t\tRESETING...\n")
             st = time.monotonic()
             p.accuracy = 0
             p.correct_guesses = 0
             p.wrong_guesses = 0
-            p.adjusted = 0
 
         ret, frame = video_capture.read()
 
